13981803b98d502d0817dd5d2dea851ee70fcc71
nisakib07 <websakib07@gmail.com>
2023-10-19 11:03:50 +0600

    Basic Setup Complete

---
+++ src/config/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjdoPv4vbwSFvNW9MxvqItSpLobM2bWws",
+  authDomain: "assignmentb8-10.firebaseapp.com",
+  projectId: "assignmentb8-10",
+  storageBucket: "assignmentb8-10.appspot.com",
+  messagingSenderId: "477949051502",
+  appId: "1:477949051502:web:30f91a4016947ee50e9c7f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
